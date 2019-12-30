3d1e824e1b5d252ed0aeb24ed64b659689b5db61
t-zochi <mojamoja.000@gmail.com>
2019-12-30 21:29:04 +0900

    update test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -18,12 +18,12 @@ multiplication_lists = (
     [0, 0, 0],
     [1, 1, 1],
     [5, 1, 5],
-    [10, 10, 1000]
+    [10, 10, 100]
 )
 
 division_lists = (
     [0, 1, 0],
-    [1, 1, 0],
+    [1, 1, 1],
     [10, 5, 2]
 )
 
@@ -70,7 +70,7 @@ def division_task(request):
     return request.param
 
 
-# def test_division_normal(division_task):
-#    cal = calculator.Calculator()
-#    result = cal.division(division_task[0], division_task[1])
-#    assert result == division_task[2]
+def test_division_normal(division_task):
+    cal = calculator.Calculator()
+    result = cal.division(division_task[0], division_task[1])
+    assert result == division_task[2]
